ef309c272e2044d50351c0b544c5350a44ba8b1b
VasanthCf <vegamvasanth11@gmail.com>
2025-06-02 21:40:10 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ import { config } from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionRouter from "./routes/transRoute.js";
 import { initDB } from "./config/db.js";
-import cors from "cors";
+
 config();
 const app = express();
 app.use(express.json());
 app.use(rateLimiter);
-app.use(cors());
+
 app.use("/api/transactions", transactionRouter);
 initDB().then(() => {
   app.listen(process.env.PORT || 3000, () => {
